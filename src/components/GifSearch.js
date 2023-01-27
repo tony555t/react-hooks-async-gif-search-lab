053374146334284c010c8495e77dcdbccedbb7c0
tony555t <tonytullah764@gmail.com>
2023-01-27 23:15:47 +0300

    asyn

---
+++ src/components/GifSearch.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+function GifListSearch({ onSubmitQuery }) {
+    const [querySearch, setQuerySearch] = useState("")
+
+    function handleSearchChange(e) {
+        setQuerySearch(e.target.value);
+    }
+
+    function handleQuerySearch(e) {
+        e.preventDefault();
+        onSubmitQuery(querySearch);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleQuerySearch}>
+                <div className="form-group">
+                    <label htmlFor="search">Enter a search Term:</label>
+                    <input
+                        id="search"
+                        className="form-control"
+                        type="text"
+                        value={querySearch}
+                        onChange={handleSearchChange}
+                    />
+                </div>
+                <button type="submit" className="btn btn-success">Find Gifs</button>
+            </form>
+        </div>
+    ) 
+}
+
+export default GifListSearch;
